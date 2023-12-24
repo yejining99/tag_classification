55af68872d4f5897d497b5280fd5c8263299ba71
mimiskims <112559136+msraask3@users.noreply.github.com>
2023-12-24 19:59:28 +0900

    Update model.py

--- model.py
+++ model.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 import loralib as lora
 
 class ContrastiveModel(nn.Module):
-    def __init__(self, LLM, tokenizer, use_lora=True, max_length=512, K=None, lora_layer=None, finetuning=False):  # Default value for lora_layer set to None
+    def __init__(self, LLM, tokenizer, use_lora=True, max_length=512, K=None, lora_layer=None, finetuning=False, freeze_layers=3):
         super(ContrastiveModel, self).__init__()
         self.LLM = LLM
-        if finetuning == False: # If we're not finetuning, freeze the weights
+        
+        if finetuning == False: 
             for param in self.LLM.parameters():
                 param.requires_grad = False
     
-        else: # If we're finetuning, freeze the first 3 layers
+        else:
             for i, layer in enumerate(self.LLM.encoder.layer):
-                if i < 20:
+                if i < freeze_layers:
                     for param in layer.parameters():
                         param.requires_grad = False
                 else:
@@ -22,10 +23,9 @@ class ContrastiveModel(nn.Module):
         self.max_length = max_length
         self.use_lora = use_lora
         self.K = K
-        self.lora_layer = lora_layer if lora_layer is not None else -1  # If None, use last hidden state
-        d_model = self.LLM.config.hidden_size  # BERT hidden size
+        self.lora_layer = lora_layer if lora_layer is not None else -1  
+        d_model = self.LLM.config.hidden_size  
 
-        # If we're using LoRA
         if self.use_lora:
             self.lora = lora.Linear(d_model, d_model, r=self.K)
 
